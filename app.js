be755aba7549f29bd7c80e631742ab9419fc891d
JesusCorderoT <jesus.of.999@gmail.com>
2023-07-06 11:04:23 -0600

    Se termina ejercicio

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
 // Escribe tu código aquí.
+
+function palabras(str , arr)
+{
+return arr.filter(palabra => palabra.length > str.length);
+
+}
+
+const array= ['insecto', 'bootcamp','mangos','reptil','mosca'];
+
+console.log(palabras('bocina',array));
+
+
+//2
+
+function imprime(arr)
+{
+    const ul = document.createElement('ul');
+
+    arr.forEach(str => {
+        const li = document.createElement('li');
+        li.textContent = str;
+        ul.appendChild(li);
+        
+    });
+
+    document.body.appendChild(ul);
+    
+
+}
+
+const arr2 = ['insecto','bootcamp','escritorio','ALGO'];
+imprime(arr2);
